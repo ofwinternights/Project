f37fd36cea0821c23d7fd2965cf47c19fd7c311e
ofwinternights <shsu14cs@cmrit.ac.in>
2017-09-14 22:23:36 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function submit_by_id() 
-{
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-document.getElementById("form_id").submit();
-alert(" Userame : " + username + " \n Password : " + password);
-}
